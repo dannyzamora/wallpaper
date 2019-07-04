240f7afcd78f413e9e0aecefc3ff0965e3a96233
Danny Zamora <dannyzamora.js@gmail.com>
2019-07-03 21:41:40 -0700

    working

--- imagescrape.py
+++ imagescrape.py
@@ -1,18 +1,64 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, SoupStrainer
 from urllib.request import Request,urlopen
+from urllib.parse import urlparse
 import ssl
+import random
+import re
+import os
 
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = "https://www.wallpapermaiden.com/category/3d?page=1"
+def Scape():
+    url = lookforimage()
+    path = saveImg(url)
+    return path
 
-req = Request(url,  headers={'User-Agent': 'Mozilla/5.0'})
+def lookforimage():
+    num = random.randint(1,54)
+    url = "https://www.wallpapermaiden.com/category/city?page="+str(num)
+    print("Starting URL:",url)
 
-html = urlopen(req, context=ctx).read()
+    req = Request(url,  headers={'User-Agent': 'Mozilla/5.0'})
+    html = urlopen(req, context=ctx).read()
+    print("Home Start....")
+    product = SoupStrainer('div',{"class":"wallpaperList"})
+    soup = BeautifulSoup(html,'html.parser',parse_only=product)
+    links = soup.find_all('a',href=True)
+    r = random.choice(links) 
+    url = r['href']
+    print("Home FIN....")
 
-soup = BeautifulSoup(html, "html.parser")
 
-print(soup)
+    for i in range(2):
+        print("Stating",i)
+        req = Request( url,  headers={'User-Agent': 'Mozilla/5.0'})
+        html = urlopen(req, context=ctx).read()
+        product = SoupStrainer('div',{"class": re.compile('wpBig *')})
+        soup = BeautifulSoup(html, "html.parser",parse_only=product)
+        imgs = soup.find('div',{"class": re.compile('wpBig *')})
+        links = imgs.find('a',href=True)
+        url = links['href']
+        print("Ending",i)
+
+    print(url)
+    return(url)
+
+def createFolder():
+    if not os.path.exists("backgrounds"):
+        os.makedirs("backgrounds")
+
+def saveImg(imgurl):
+    createFolder()
+    print(imgurl)
+    print(urlparse(imgurl).path)
+    path = urlparse(imgurl).path
+    path=os.path.split(path)
+    path = "backgrounds/"+path[1]
+    req = Request(imgurl,  headers={'User-Agent': 'Mozilla/5.0'})
+    open(path, "wb").write(urlopen(req).read())
+    return path
+
+
--- wallpaper.py
+++ wallpaper.py
@@ -1,9 +1,11 @@
 import os
 import ctypes
+import imagescrape
 
-name_of_file = 'color.png'
+
+name_of_file = imagescrape.Scape()
 path_to_file = os.path.realpath(name_of_file) # Returns the canonical path
 
-print(path_to_file) # this printa C:\Users\WORK\Desktop\projects\wallpaper\wallpaper\color.png
+print(path_to_file) # this printasC:\Users\WORK\Desktop\projects\wallpaper\wallpaper\color.png
 SPI_SETDESKWALLPAPER = 20
 ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, path_to_file, 0)
